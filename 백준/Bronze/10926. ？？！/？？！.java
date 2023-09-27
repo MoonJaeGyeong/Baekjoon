7311f0b245e0d78199c193e521d16c481cb0f012
MoonJaeGyeong <bblbblan69@gmail.com>
2023-09-27 16:02:07 +0900

    [Bronze V] Title: ??!, Time: 212 ms, Memory: 17744 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String scan = sc.nextLine();
+        
+
+        System.out.println(solution(scan));
+    }
+
+    public static String solution(String s){
+        return s + "??!";
+    }
+}
