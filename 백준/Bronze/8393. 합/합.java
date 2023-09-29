0170e528cf9ada58a7e5789af2bc9f8c9c837cd0
MoonJaeGyeong <bblbblan69@gmail.com>
2023-09-29 21:39:19 +0900

    [Bronze V] Title: 합, Time: 204 ms, Memory: 17640 KB -BaekjoonHub

---
+++ 백준/Bronze/8393. 합/합.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int n = sc.nextInt();
+        int sum = 0;
+
+        for(int i=1; i<=n; i++){
+          sum += i;
+        }
+
+        System.out.println(sum);
+
+    }
+
+}
