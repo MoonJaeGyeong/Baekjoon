155f853e00591004d184626d8129edcf2335e558
MoonJaeGyeong <bblbblan69@gmail.com>
2023-09-29 21:46:31 +0900

    [Bronze V] Title: 코딩은 체육과목 입니다, Time: 224 ms, Memory: 17776 KB -BaekjoonHub

---
+++ 백준/Bronze/25314. 코딩은 체육과목 입니다/코딩은 체육과목 입니다.java
@@ -0,0 +1,19 @@
+import javax.xml.transform.Source;
+import java.util.Scanner;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+       int n = sc.nextInt()/4;
+
+       for(int i=0; i<n; i++){
+           System.out.printf("long ");
+       }
+
+        System.out.println("int");
+    }
+
+}
