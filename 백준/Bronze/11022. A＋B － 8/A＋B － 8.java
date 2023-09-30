31078ea20b5a04bd446bebd1982f1e1acc2207fd
MoonJaeGyeong <bblbblan69@gmail.com>
2023-09-30 18:51:57 +0900

    [Bronze V] Title: A+B - 8, Time: 184 ms, Memory: 16676 KB -BaekjoonHub

---
+++ 백준/Bronze/11022. A＋B － 8/A＋B － 8.java
@@ -0,0 +1,25 @@
+import java.io.*;
+
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) throws IOException{
+        String arr[] = new String[2];
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        int n = Integer.parseInt(br.readLine());
+
+        for(int i=1; i<=n; i++){
+                arr = br.readLine().split(" ");
+                int sum = Integer.parseInt(arr[0]) + Integer.parseInt(arr[1]);
+                bw.write("Case #" + i + ": " + arr[0] + " + " + arr[1] + " = " + String.valueOf(sum));
+                bw.newLine();
+
+        }
+        bw.flush();
+        bw.close();
+    }
+
+
+
+}
