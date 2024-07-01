b8891b231227e6b846ef8e9a6cd1e0bcb10bf350
MoonJaeGyeong <bblbblan69@gmail.com>
2024-07-02 03:22:48 +0900

    [Bronze V] Title: 와이버스 부릉부릉, Time: 156 ms, Memory: 17476 KB -BaekjoonHub

---
+++ 백준/Bronze/14645. 와이버스 부릉부릉/와이버스 부릉부릉.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class Main{
+    public static void main(String[] args){
+        
+        Scanner sc = new Scanner(System.in);
+        int n,k;
+        int a,b;
+        n = sc.nextInt();
+        k = sc.nextInt();
+        
+        for(int i=0; i<n; i++){
+            a = sc.nextInt();
+            b = sc.nextInt();
+        }
+        
+        System.out.println("비와이");
+    }
+}
