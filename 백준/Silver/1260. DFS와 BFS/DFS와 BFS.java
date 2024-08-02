53fd3c271ede7bda6db1bd5ce912ba596c89dbe3
MoonJaeGyeong <bblbblan69@gmail.com>
2024-08-02 16:36:32 +0900

    [Silver II] Title: DFS와 BFS, Time: 672 ms, Memory: 41256 KB -BaekjoonHub

--- 백준/Silver/1260. DFS와 BFS/DFS와 BFS.java
+++ 백준/Silver/1260. DFS와 BFS/DFS와 BFS.java
@@ -1,29 +1,33 @@
 import java.io.IOException;
-import java.util.LinkedList;
-import java.util.Queue;
-import java.util.Scanner;
+import java.util.*;
 
 public class Main {
     static int N,M;
-    static int[][] graph;
+    static ArrayList<LinkedList<Integer>> graph;
     static int start;
     static boolean[] visited;
-    static int[] dfs;
-    static int[] bfs;
 
     public static void main(String[] args) throws IOException {
         Scanner sc = new Scanner(System.in);
         N = sc.nextInt();
         M = sc.nextInt();
         start = sc.nextInt() - 1;
-        graph = new int[N][N];
+        graph = new ArrayList<>();
         visited = new boolean[N];
+
+        for(int i=0; i<N; i++){
+            graph.add(new LinkedList<>());
+        }
 
         for(int i=0; i<M; i++){
             int row = sc.nextInt() - 1;
             int col = sc.nextInt() - 1;
-            graph[row][col] = 1;
-            graph[col][row] = 1;
+            graph.get(row).add(col);
+            graph.get(col).add(row);
+        }
+
+        for(int i=0; i<N; i++){
+            Collections.sort(graph.get(i));
         }
 
         makedfs(start);
@@ -35,10 +39,11 @@ public class Main {
         visited[index] = true;
         System.out.print(index+1 + " ");
 
-        for(int i=0; i<N; i++){
-            if(graph[index][i] == 1 && !visited[i]){
-                visited[i] = true;
-                makedfs(i);
+        for(int i=0; i<graph.get(index).size(); i++){
+            int n = graph.get(index).get(i);
+            if(!visited[n]){
+                visited[n] = true;
+                makedfs(n);
             }
         }
     }
@@ -53,10 +58,11 @@ public class Main {
         while (!queue.isEmpty()){
             int node = queue.poll();
 
-            for(int i=0; i<N; i++){
-                if(graph[node][i] == 1 && !queue.contains(i) && !visited[i]){
-                    queue.add(i);
-                    visited[i] = true;
+            for(int i=0; i<graph.get(node).size(); i++){
+                int n = graph.get(node).get(i);
+                if(!visited[n]){
+                    queue.add(n);
+                    visited[n] = true;
                 }
             }
 
